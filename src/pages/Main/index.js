143e95f8a1594d4ed6ff27b7ad148fb6322adce0
Artur Della Favera <arturdellafavera@gmail.com>
2020-03-17 14:44:40 -0300

    FE-CLIENT-DEV / FEATURE / Recebe tags de estilização da pagina

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { Title } from './styles';
+import { Container, Form, SubmitButton } from './styles';
+
+import { FaGithubAlt, FaPlus } from 'react-icons/fa'
 /** Cria componente Main */
 export default function Main() {
   return (
-    <Title>
-      Hello World
-    </Title>
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input
+          type="text"
+          placeholder="Adicionar repositório"
+        />
+        <SubmitButton disabled>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
   );
 }
